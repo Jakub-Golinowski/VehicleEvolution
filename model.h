4ef897bf12341d01ac561ba97d788aa1d6d5e8c6
Jakub-Golinowski <jakub.golinowski@gmail.com>
2017-06-05 19:24:51 +0200

    Updated model.h Documentation still not finished.

--- model.h
+++ model.h
@@ -28,14 +28,15 @@ public:
      */
     b2Body* addRectBody(float posX, float posY, b2BodyType bodyType, float angle_radians);
 
+    /*!
+     * \brief Adds track used to evaluate cars.
+     */
     void addTrack();
 
     b2Body* addWheelBody(float poxX, float posY);
     b2Fixture* addWheelFixture(b2Body* parentBody, float radius, float density, float friction, float restitution, uint16 collisionGroup);
     b2Fixture* addCircleFixture(b2Body *parentBody, float posX, float posY, float radius,
                                        float density, float friction, float restitution);
-
-    b2Joint* addRevoluteJoint(b2RevoluteJointDef* revoluteJointDef);
 
     b2Body* addCarFromChromosome(Chromosome chromosome, float posX, float posY);
 
@@ -48,17 +49,24 @@ public:
 private:
     /*!
      * \brief Creates ground chain shape which is a track used to evaluate cars.
-     * \param parentBody
-     * \param points
-     * \param pointsCount
-     * \param density
-     * \param friction
-     * \param restitution
-     * \param collisionGroup
-     * \return
+     * \param parentBody body to which ground chain shape will be added.
+     * \param points pointer to an array of points that define chain shape.
+     * \param pointsCount number of points in points array.
+     * \param density physics parameter (mass/volume) used for box2D simulation.
+     * \param friction physics parameter used for box2D simulation.
+     * \param restitution physics paramtere used for box2D simulation
+     * \param collisionGroup number denoting a group to which shape will belong. Shapes in the same group of negative value do not collide.
+     * \return Chain shape fixture.
      */
     b2Fixture* addGroundChainShape(b2Body *parentBody, b2Vec2 * points, unsigned int pointsCount,
                                        float density, float friction, float restitution, uint16 collisionGroup);
+
+    /*!
+     * \brief Adds revolute joint to the car model.
+     * \param revoluteJointDef a data structer that contains revolute joint parameters.
+     * \return pointer to the newly created joint.
+     */
+    b2Joint* addRevoluteJoint(b2RevoluteJointDef* revoluteJointDef);
 
     static const float32 BOX2D_TIMESTEP;
     static const int32 BOX2D_VELOCITY_ITERATIONS;
